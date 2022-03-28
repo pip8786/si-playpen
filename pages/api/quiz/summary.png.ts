a8bdfe0565573783c04b0ba5b861b122e52ca37b
Philippe Sabourin <philippe.sabourin@sas.com>
2022-03-28 10:53:15 -0400

    Trying just the system font

--- pages/api/quiz/summary.png.ts
+++ pages/api/quiz/summary.png.ts
@@ -40,17 +40,17 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
                     fill: #181818;
                 }
                 .cls-12, .cls-3 {
-                    font-family: Inter,sans-serif;
+                    font-family: sans-serif;
                     font-weight: 500;
                 }
                 .cls-5, .cls-7 {
                     font-size: 27px;
                 }
                 .cls-5 {
-                    font-family: Inter,sans-serif;
+                    font-family: sans-serif;
                 }
                 .cls-13, .cls-15, .cls-7 {
-                    font-family: Inter,sans-serif;
+                    font-family: sans-serif;
                     font-weight: 700;
                 }
                 .cls-12, .cls-15 {
