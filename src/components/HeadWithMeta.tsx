208da25d11c78aec066752ec6d0e167ac053706f
Philippe Sabourin <philippe.sabourin@gmail.com>
2022-04-04 16:00:29 -0400

    Adding meta for non results quiz

--- src/components/HeadWithMeta.tsx
+++ src/components/HeadWithMeta.tsx
@@ -24,6 +24,20 @@ export const HeadWithMeta:React.FC<{title:string}> = ({children, title}) => {
 			<meta property="og:description"   content={`My curiosity score is ${totalAnswered}. What's yours? Take this simple quiz to find out.`}/>
 			<meta property="og:image"         content={`${process.env.NEXT_PUBLIC_BASE_URL}/api/quiz/gauge.png?min=${totalMin}&max=${totalPossible}&value=${totalAnswered}`} />
 		</>;
+	} else {
+		metaTags = <>
+			<meta name="twitter:card" content="summary_large_image"/>
+			<meta name="twitter:site" content="@SASsoftware"/>
+			<meta name="twitter:creator" content="@saseducator"/>
+			<meta name="twitter:title" content="Curiosity at Work Quiz"/>
+			<meta name="twitter:description" content="What's your curiosity score? Take this simple quiz to find out."/>
+			<meta name="twitter:image" content={`${process.env.NEXT_PUBLIC_BASE_URL}/images/${experience.shortcode}/1.png`}/>
+			<meta property="og:url"           content={`${process.env.NEXT_PUBLIC_BASE_URL}/${experience.shortcode}/`} />
+			<meta property="og:type"          content="website" />
+			<meta property="og:title"         content="Curiosity at Work Quiz" />
+			<meta property="og:description"   content="What's your curiosity score? Take this simple quiz to find out."/>
+			<meta property="og:image"         content={`${process.env.NEXT_PUBLIC_BASE_URL}/images/${experience.shortcode}/1.png`} />
+		</>;
 	}
 
 	return (
