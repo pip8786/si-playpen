b7924ac886f0166c98708fc239043939796f6dc1
Hailey Turner <Hailey.Turner@sas.com>
2022-04-28 21:37:08 -0400

    removed any not needed imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import type { NextPage } from 'next';
 import Head from "next/head";
-import {ExperienceWithContent, getExperience} from "./api/experience/[shortcode]";
+import {getExperience} from "./api/experience/[shortcode]";
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Link from 'src/components/Link';
-import {Button, Fade, Stack} from "@mui/material";
+import {Button} from "@mui/material";
 import Paper from "@mui/material/Paper";
 import Image from 'next/image';
 import {useRouter} from "next/router";
@@ -19,8 +19,6 @@ const Home: NextPage = ({experience}) => {
 
     const router = useRouter();
     const [loading, setLoading] = useState(false);
-    console.log(experience)
-
 
     const loadingCuriousExperience = () => {
         //onClick for "Launch Quiz", loading indicator occurs while waiting to be pushed to '/curious' page
