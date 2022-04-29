8cf55fd59b2e8c22b7b00dab209cc463c6798556
Hailey Turner <Hailey.Turner@sas.com>
2022-04-28 21:26:19 -0400

    design updates to 404 and 500 pages

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,8 +3,12 @@ import Paper from "@mui/material/Paper";
 import Container from '@mui/material/Container';
 import Link from 'next/link';
 import Head from "next/head";
+import {Button} from "@mui/material";
+import {useRouter} from "next/router";
 
-export default function Custom404() {;
+export default function Custom404() {
+
+    const router = useRouter();
 
     return (
 
@@ -24,11 +28,30 @@ export default function Custom404() {;
                     m:1
                 }}
             >
-                 <Head><title>404 - Page Not Found</title></Head>
-                 <Typography variant="h1" align="center">404 - Page Not Found</Typography>
-                 <br></br>
-                 <Typography variant="h4" align="center">Return to the <Link href="/curious">homepage</Link></Typography>
-
+                     <Head><title>404 - Page Not Found</title></Head>
+                     <Typography variant="h1" align="center" fontWeight="bold">404</Typography>
+                     <Typography variant="h2" align="center" >Page Not Found</Typography>
+                     <Typography variant="body1" align="center" >Opps! Sorry, the page you were looking for does not exist.</Typography>
+                                      <Container
+                                          sx={{
+                                          display: 'flex',
+                                          justifyContent: 'center',
+                                          alignItems:'center',
+                                          pt: '30px'
+                                          }}
+                                      >
+                                          <Button
+                                             sx = {{
+                                                   height: '60px',
+                                                   width: '200px',
+                                             }}
+                                             variant="contained"
+                                             size="large"
+                                             onClick={()=>router.push("/")}
+                                         >
+                                         Return To Home
+                                         </Button>
+                                     </Container>
             </Paper>
         </Container>
 
--- pages/500.tsx
+++ pages/500.tsx
@@ -3,9 +3,13 @@ import Paper from "@mui/material/Paper";
 import Container from '@mui/material/Container';
 import Link from 'next/link';
 import Head from "next/head";
+import {Button} from "@mui/material";
+import {useRouter} from "next/router";
 
 
-export default function Custom500() {;
+export default function Custom500() {
+
+    const router = useRouter();
 
     return (
 
@@ -25,10 +29,31 @@ export default function Custom500() {;
                     m:1
                 }}
             >
-                 <Head><title>500 - Server Error</title></Head>
-                 <Typography variant="h1" align="center">500 - Server Error</Typography>
-                 <br></br>
-                 <Typography variant="h4" align="center">Return to the <Link href="/curious">homepage</Link></Typography>
+                 <Head><title>500 - Internal Server Error</title></Head>
+                 <Typography variant="h1" align="center" fontWeight="bold">500</Typography>
+                 <Typography variant="h2" align="center">Internal Server Error</Typography>
+                 <Typography variant="body1" align="center">Oh No! Sorry, there was an unexpected error. </Typography>
+
+                 <Container
+                     sx={{
+                     display: 'flex',
+                     justifyContent: 'center',
+                     alignItems:'center',
+                     pt: '30px'
+                     }}
+                 >
+                     <Button
+                        sx = {{
+                              height: '60px',
+                              width: '200px',
+                        }}
+                        variant="contained"
+                        size="large"
+                        onClick={()=>router.push("/")}
+                    >
+                    Return To Home
+                    </Button>
+                </Container>
 
 
 
