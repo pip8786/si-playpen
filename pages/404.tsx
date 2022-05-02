84ec9ebe39a10e2be1f6cb4f04cc5f59b41ce94e
Hailey Turner <Hailey.Turner@sas.com>
2022-05-02 14:31:29 -0400

    fixed type error

--- pages/404.tsx
+++ pages/404.tsx
@@ -9,7 +9,7 @@ import {ErrorPages} from 'src/components/ErrorPages';
 
 export default function Custom404() {
 
-    const errorNum = '404'
+    const errorNum = 404
     const errorName = 'Page Not Found'
     const errorSlogan = "Opps! Sorry, the page you were looking for does not exist."
 
