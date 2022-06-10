33f675467b3cfc8dc7bb66b815a039ba8996541f
Philippe Sabourin <philippe.sabourin@sas.com>
2022-06-10 14:31:30 -0400

    Fixing up the auto sizing of the questions on the quiz

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,17 +1,17 @@
 import * as React from "react";
+import {useContext, useRef, useState} from "react";
 import Image from "next/image";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
 import {Button, Fade, Stack} from "@mui/material";
 import CircularProgress from "@mui/material/CircularProgress"
-import {useRef, useContext, useState, useEffect} from "react";
 import {QuizContext} from "src/context/QuizContext";
 import {Box} from "@mui/system";
 import {ExperienceContext} from "../context/ExperienceContext";
 import {useRouter} from "next/router";
 import {HeadWithMeta} from "./HeadWithMeta";
-import { useDimensionObserver } from "src/hooks/useDimensionObserver";
+import {useDimensionObserver} from "src/hooks/useDimensionObserver";
 
 export const Quiz = () => {
     const router = useRouter();
@@ -23,13 +23,13 @@ export const Quiz = () => {
     const {width, height} = useDimensionObserver(el);
 
 
-    const answer = async (index:number) => {
+    const answer = async (index: number) => {
 
-        if(currentQIndex === quiz.questions.length - 1) {
+        if (currentQIndex === quiz.questions.length - 1) {
             setLoading(true);
             const newValue = [...answers, index];
-            const score = newValue.reduce((t, a, i) => t+quiz.questions[i].answers[a].value,0);
-            const response = await fetch(`/api/experience/${experience.shortcode}`,{
+            const score = newValue.reduce((t, a, i) => t + quiz.questions[i].answers[a].value, 0);
+            const response = await fetch(`/api/experience/${experience.shortcode}`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
@@ -42,7 +42,7 @@ export const Quiz = () => {
             });
 
             const results = response.ok && await response.json();
-            if(results) {
+            if (results) {
                 await router.push(`/${experience.shortcode}/results/${results.id}`);
                 resetContext();
             } else {
@@ -54,23 +54,16 @@ export const Quiz = () => {
     }
 
 
-    
     return (
-        <Container ref={el}
-                   sx={{
-                    maxWidth: {
-                        xs: "xs",
-                        sm: "sm",
-                        md: "md",
-                        lg: "md",
-                        xl: "md"
-                       },
-                       my: 4,
-                       display: "flex",
-                       flexDirection: "column",
-                       justifyContent: "center",
-                       alignItems: "center"
-                   }}
+        <Container
+            sx={{
+                maxWidth: {xs: "xs", sm: "sm", md: "md", lg: "md", xl: "md"},
+                my: 4,
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center"
+            }}
         >
             <HeadWithMeta title="Curiosity @ Work Quiz"/>
             <Typography variant="h2" component="h1" fontWeight={500}>{experience.name}</Typography>
@@ -78,42 +71,52 @@ export const Quiz = () => {
             <Paper
                 elevation={3}
                 sx={{
-                    p:5,
-                    m:1
+                    position: "relative",
+                    height: height,
+                    alignSelf: "stretch"
                 }}
             >
-
-            <Box position="relative" width={Math.round(width*0.587)} height={height*.75}>
-                    {quiz.questions.map((q, i) => (
+                {quiz.questions.map((q, i) => (
                     <Fade in={currentQIndex === i} key={i} timeout={1000}>
-                        <Box position="absolute" top={0} right={0} left={0}>
-                            <Image src={`/images/${experience.shortcode}/${i+1}.png`} width={500} height={333} alt="Girl with Magnifying Glass"/>
-                            <Typography variant="h6" my={2}>{q.text}</Typography>
+                        <Box position="absolute" ref={el} sx={{
+                            top: 0,
+                            left: 0,
+                            right: 0
+                        }}>
+                            <Box p={6}>
+                                <Box sx={{display: "flex", justifyContent: "center"}}>
+                                    <Image src={`/images/${experience.shortcode}/${i + 1}.png`} width={500} height={333}
+                                           alt="Girl with Magnifying Glass"/>
+                                </Box>
+                                <Typography variant="h6" my={2}>{q.text}</Typography>
                                 {/*if loading is true, show loading circle; if false, show quiz questions */}
                                 {loading
                                     ? <Container
                                         sx={{
-                                        display: "flex",
-                                        justifyContent: "center",
-                                        flexDirection:"column",
-                                        alignItems:"center"
+                                            display: "flex",
+                                            justifyContent: "center",
+                                            flexDirection: "column",
+                                            alignItems: "center"
                                         }}
-                                        >
+                                    >
                                         <CircularProgress size={100}/>
                                         <Typography variant="body1">Loading your results...</Typography>
-                                        </Container>
+                                    </Container>
                                     : <Stack ref={otherEl} alignItems="stretch" spacing={2}>
                                         {q.answers.map((a, index) => (
-                                            <Button variant="contained" key={a.text} onClick={()=>answer(index)}>{a.text}</Button>
+                                            <Button variant="contained" key={a.text}
+                                                    onClick={() => answer(index)}>{a.text}</Button>
                                         ))
                                         }
-                                        <Typography marginTop={2} textAlign="center">{currentQIndex+1} of {quiz.questions.length}</Typography>
-                                        </Stack>
+                                        <Typography marginTop={2}
+                                                    textAlign="center">{currentQIndex + 1} of {quiz.questions.length}</Typography>
+                                    </Stack>
                                 }
                             </Box>
-                        </Fade>
-                        ))}
-                </Box>
+
+                        </Box>
+                    </Fade>
+                ))}
             </Paper>
         </Container>
 
